a391ea65d795e33738704e8233003a52b5555182
{Erfun76} <{zeinivand2012@gmail.com}>
2020-12-06 07:50:19 +0000

    First commit

---
+++ AGD_SR/search/util_gan/norm.py
@@ -0,0 +1,39 @@
+class UnNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized image.
+        """
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.mul_(s).add_(m)
+            # The normalize code -> t.sub_(m).div_(s)
+        return tensor
+
+class Normalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized image.
+        """
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.sub_(m).div_(s)
+        return tensor
+
+def stat_calc(img):
+    img = img.view(img.size(0), img.size(1), -1)
+    mean  = img.mean(2).sum(0) 
+    std   = img.std(2).sum(0)
+
+    return mean, std
